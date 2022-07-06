6e8d788ced1bbce5b6f4beb299821b2dc45bfffe
Fredericks80 <95603728+Fredericks80@users.noreply.github.com>
2022-07-06 17:01:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 let fs = require('fs')                    //CREATOR = GEMPY , DEVBOT
 let chalk = require('chalk')
 global.owner = [
-  ['6287898307350'],
-  ['66287898307350'],
-  ['6287898307350', 'gempy', true]  
+  ['629512339018'],
+  ['629512339018'],
+  ['629512339018', 'frederick', true]  
   
 ] 
-global.ownername = [['Gempy']]
-global.namabot = [['PENTABOT']]
-global.ownerinsta = [['@gmpyyy']]
+global.ownername = [['Mangtaf']]
+global.namabot = [['MANGTAFBOT']]
+global.ownerinsta = [['@fredericks_27']]
 global.mods = [] 
 global.prems = [] 
 
@@ -43,8 +43,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'PENTABOT'
-  var sticker_author = 'By GEMPY'
+  var sticker_name = 'MANGTAFBOT'
+  var sticker_author = 'By Mangtaf'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
